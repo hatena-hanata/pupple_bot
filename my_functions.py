55402d3f0bd79b67f280def3e254014a7f5e52a8
haten-hanata <hanatasfp@gmail.com>
2019-09-23 15:48:01 +0900

    test reading static file

--- my_functions.py
+++ my_functions.py
@@ -3,10 +3,12 @@ from bs4 import BeautifulSoup
 
 
 def scraping(input_text):
-    r = requests.get(input_text)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    # 曲名取得
-    song_name = soup.find('title').text.split('ギ')[0]
-    song_name = song_name[:len(song_name) - 1]
+    # r = requests.get(input_text)
+    # soup = BeautifulSoup(r.content, 'html.parser')
+    # # 曲名取得
+    # song_name = soup.find('title').text.split('ギ')[0]
+    # song_name = song_name[:len(song_name) - 1]
+    with open('./stafile.txt', 'r') as f:
+        song_name = f.read()
 
     return song_name
