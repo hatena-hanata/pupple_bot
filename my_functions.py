6f83bc4b73a869dbed1d47672c6b469f2af05adc
haten-hanata <hanatasfp@gmail.com>
2019-09-23 16:04:59 +0900

    hitomazu?

--- my_functions.py
+++ my_functions.py
@@ -1,14 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
-
+import pandas as pd
+from music_class import *
+import pickle
 
 def scraping(input_text):
-    # r = requests.get(input_text)
-    # soup = BeautifulSoup(r.content, 'html.parser')
-    # # 曲名取得
-    # song_name = soup.find('title').text.split('ギ')[0]
-    # song_name = song_name[:len(song_name) - 1]
-    with open('./staticfiles/stafile.txt', 'r') as f:
-        song_name = f.read()
+    r = requests.get(input_text)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    # 曲名取得
+    song_name = soup.find('title').text.split('ギ')[0]
+    song_name = song_name[:len(song_name) - 1]
 
-    return song_name
+    # Songインスタンス作成
+    song = Song(song_name, Chord('C'))
+
+    # コードを読み取る
+    chord_lst = soup.find_all('rt')
+    for c in chord_lst:
+        chord_str = c.text
+        if c.text[0] == 'N':
+            continue
+        chord = Chord(chord_str)
+        song.append_chord(chord)
+
+    # 学習データ作成
+    df = song.to_DataFrame()
+    df = df[df.columns[2:]]
+    for col in df.columns:
+        df[col] = df[col].astype(float)
+
+    # 学習済みモデル読み込み
+    loaded_model = pickle.load(open('./staticfiles/model.pkl', 'rb'))
+    le = pickle.load(open('staticfiles/le.pkl', 'rb'))
+
+    # 予測
+    pred_y_value = loaded_model.predict(df)
+    pred_y_label = le.inverse_transform(pred_y_value)[0]
+
+    if pred_y_label.split('_')[-1] == 'Major':
+        answer = pred_y_label.split('_')[0]
+    else:
+        answer = pred_y_label.split('_')[0] + 'm'
+
+    result = '{} のキーは {} です！'.format(song_name, answer)
+
+    return result
+
