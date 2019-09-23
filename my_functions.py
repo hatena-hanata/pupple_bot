80120b6164d3fd0461c10129d0f4886576a8795a
haten-hanata <hanatasfp@gmail.com>
2019-09-23 15:39:46 +0900

    song_name

--- my_functions.py
+++ my_functions.py
@@ -3,4 +3,10 @@ from bs4 import BeautifulSoup
 
 
 def scraping(input_text):
-    return input_text + 'ありがとう'
+    r = requests.get(input_text)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    # 曲名取得
+    song_name = soup.find('title').text.split('ギ')[0]
+    song_name = song_name[:len(song_name) - 1]
+
+    return song_name
